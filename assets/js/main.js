ee898212b653408e4173a1734cbe107bbd644591
Annelise Claire <anclaire@davidson.edu>
2021-09-12 18:20:36 -0400

    Final Commit (??)

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,13 @@ Staaatliches Bauhaus (1919-1933)
 console.log(bauhaus);
 
 console.log('My potrait looks really sad, but that was an accident. Dont know how to make it look happier...');
+console.log('total number of HTML elements:');
+console.log(document.querySelectorAll('*').length);
+
+console.log('I found the x y coordinate help thanks to this Gavs Blog site: https://www.gavsblog.com/blog/get-the-current-position-of-the-mouse-from-a-javascript-event');
+document.addEventListener('mousemove', (event) => {
+});
+
+document.addEventListener('mousemove', (event) => {
+	console.log(`Mouse X: ${event.clientX}, Mouse Y: ${event.clientY}`);
+});
